50364cfd4b018c196217bedcbf979a8d61a948c6
Brendo Jair Puse Del Campo Arenas <brendo.arenas@mns.hemoam>
2023-05-22 17:11:15 -0400

    Interface de alert na Tela gerado e funcionando

--- comunication/interface.py
+++ comunication/interface.py
@@ -1,18 +1,10 @@
 import PySimpleGUI as sg
+import win32gui, win32con
 
-# Define the window's contents
-layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
-            [sg.Input()],
-            [sg.Button('Ok')] ]
 
-# Create the window
-window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
+sg.theme('dark grey 9')
+sg.popup('Sua Postura Esta incorreta')
 
-# Display and interact with the Window
-event, values = window.read()                   # Part 4 - Event loop or Window.read call
 
-# Do something with the information gathered
-print('Hello', values[0], "! Thanks for trying PySimpleGUI")
-
-# Finish up by removing from the screen
-window.close()                                  # Part 5 - Close the Window
+sg = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(sg , win32con.SW_HIDE)
