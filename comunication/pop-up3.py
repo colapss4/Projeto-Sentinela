fb3234fec5c487c9987e49cca9dfb84d4b21025d
Brendo Jair Puse Del Campo Arenas <brendo.arenas@mns.hemoam>
2023-05-23 17:51:03 -0400

    att pop-ups sem borda na pasta execs-no-border

---
+++ comunication/pop-up3.py
@@ -0,0 +1,25 @@
+import serial
+import PySimpleGUI as sg
+import time
+
+
+# Defini o tema da interface gráfica
+sg.theme('dark grey 9')
+
+# Configurar a porta serial
+porta_serial = serial.Serial('COM3', 9600)  # Altere para a porta serial correta
+
+while True:
+    # Verificar se há dados disponíveis na porta serial
+    if porta_serial.in_waiting > 0:
+        # Ler os dados da porta serial
+        dado = porta_serial.read().decode().strip()
+
+        # Verificar se o sinal do Arduino foi recebido
+        if dado == '1':
+            # Exibir o popup
+            sg.popup_no_titlebar('Sua Postura Está Incorreta')   
+
+    # Adicionar um pequeno atraso para não consumir muito processamento
+    time.sleep(0.1)
+
